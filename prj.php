1a953c9045d08238e6c38812c53d9ecf75984575
Grecu Liviu <110229478+Liviu-sourceCode@users.noreply.github.com>
2024-04-23 10:16:53 +0300

    Update prj.php

--- prj.php
+++ prj.php
@@ -15,9 +15,10 @@ if ($conn->connect_error) {
 
 //se verifica daca exista vreo cerere de tip POST
 if (($_SERVER["REQUEST_METHOD"])  == "POST") {
+    
 //zona pentru gestionarea animalelor
 
-    // Adăugare animal
+    // Adaugare animal
     if (isset($_POST["adauga_animal"])) {
         $nume_animal = $_POST["nume_animal"];
         $specie = $_POST["specie"];
@@ -25,9 +26,9 @@ if (($_SERVER["REQUEST_METHOD"])  == "POST") {
   
         $sql = "INSERT INTO animale (nume, specie, varsta) VALUES ('$nume_animal', '$specie', $varsta)";
         if ($conn->query($sql) === TRUE) {
-            // Dacă inserarea a fost reușită, extragem ID-ul animalului adăugat
+            // Daca inserarea a fost reusita, extragem ID-ul animalului adaugat
         $last_inserted_id = $conn->insert_id;
-        echo "Animalul a fost adăugat cu succes! ID-ul animalului este: " . $last_inserted_id . "<br>";
+        echo "Animalul a fost adaugat cu succes! ID-ul animalului este: " . $last_inserted_id . "<br>";
             
         } else {
             echo "Eroare: " . $sql . "<br>" . $conn->error;
@@ -35,25 +36,25 @@ if (($_SERVER["REQUEST_METHOD"])  == "POST") {
     }
 
 
-    // Ștergere animal
+    // Stergere animal
     if (isset($_POST["sterge_animal"])) {
         $id_animal = $_POST["id_animal"];
 
-        // Verificăm dacă ID-ul există în tabel
+        // Verificam daca ID-ul exista in tabel
         $check_query = "SELECT id FROM animale WHERE id = $id_animal";
         $check_result = mysqli_query($conn, $check_query);
 
         if (mysqli_num_rows($check_result) > 0) {
-            // ID-ul există în tabel, efectuăm operația de ștergere
+            // ID-ul exista in tabel, efectuam operatia de stergere
             $delete_query = "DELETE FROM animale WHERE id = $id_animal";
             if (mysqli_query($conn, $delete_query)) {
-                echo "Animalul cu ID-ul $id_animal a fost șters cu succes!<br>";
+                echo "Animalul cu ID-ul $id_animal a fost sters cu succes!<br>";
             } else {
-                echo "Eroare la ștergerea animalului: " . mysqli_error($conn);
+                echo "Eroare la stergerea animalului: " . mysqli_error($conn);
             }
         } else {
-            // ID-ul nu există în tabel, afișăm un mesaj corespunzător
-            echo "Nu există nici un animal cu ID-ul $id_animal!<br>";
+            // ID-ul nu exista in tabel, afisam un mesaj corespunzător
+            echo "Nu exista nici un animal cu ID-ul $id_animal!<br>";
         }
     } 
 
@@ -61,7 +62,7 @@ if (($_SERVER["REQUEST_METHOD"])  == "POST") {
     if(isset($_POST["sterge_toate_animalele"])){
     $sql = "DELETE FROM animale";
     if (mysqli_query($conn, $sql)) {
-    echo "Toate datele din tabelul 'animale' au fost șterse cu succes!<br>";
+    echo "Toate datele din tabelul 'animale' au fost sterse cu succes!<br>";
     $reset_auto_increment = "ALTER TABLE animale AUTO_INCREMENT = 1";
 
 // Executarea comenzii SQL
@@ -71,7 +72,7 @@ if (mysqli_query($conn, $reset_auto_increment)) {
     echo "Eroare la resetarea incrementului: " . mysqli_error($conn);
 }
 } else {
-    echo "Eroare la ștergerea datelor din tabelul 'animale': " . mysqli_error($conn);
+    echo "Eroare la stergerea datelor din tabelul 'animale': " . mysqli_error($conn);
 }
     }
 
@@ -80,7 +81,6 @@ $sql_select_animale = "SELECT * FROM animale";
 $result = $conn->query($sql_select_animale);
 
 // Verifica daca interogarea a returnat rezultate
-
 if ($result->num_rows > 0 && (isset($_POST["adauga_animal"]) || isset($_POST["sterge_animal"])))  {
     echo "Elementele din tabelul 'animale':<br>";
     // Afiseaza rezultatele
@@ -104,7 +104,7 @@ if (mysqli_query($conn, $reset_auto_increment)) {
 
 // zona pentru gestionarea vaccinurilor
 
-    // Adăugare vaccin
+    // Adaugare vaccin
     if (isset($_POST["adauga_vaccin"])) {
         $nume_vaccin = $_POST["nume_vaccin"];
         $tip_vaccin = $_POST["tip_vaccin"];
@@ -113,17 +113,17 @@ if (mysqli_query($conn, $reset_auto_increment)) {
 
         $sql = "INSERT INTO vaccinuri (nume, tip) VALUES ('$nume_vaccin', '$tip_vaccin')";
         if ($conn->query($sql) === TRUE) {
-            echo "Vaccinul a fost adăugat cu succes!";
+            echo "Vaccinul a fost adaugat cu succes!";
 
-            // Extragem ID-ul vaccinului adăugat
+            // Extragem ID-ul vaccinului adaugat
             $last_inserted_id = $conn->insert_id;
             echo " ID-ul vaccinului este: " . $last_inserted_id . "<br>";
         } else {
-            echo "Eroare la adăugarea vaccinului: " . $conn->error;
+            echo "Eroare la adaugarea vaccinului: " . $conn->error;
         }
     }
 
-    // Ștergere vaccin
+    // Stergere vaccin
     if (isset($_POST["sterge_vaccin"])) {
         $id_vaccin = $_POST["id_vaccin"];
 
@@ -133,20 +133,20 @@ if (mysqli_query($conn, $reset_auto_increment)) {
         if (mysqli_num_rows($check_result) > 0) {
             $delete_query = "DELETE FROM vaccinuri WHERE id = $id_vaccin";
             if (mysqli_query($conn, $delete_query)) {
-                echo "Vaccinul cu ID-ul $id_vaccin a fost șters cu succes!<br>";
+                echo "Vaccinul cu ID-ul $id_vaccin a fost sters cu succes!<br>";
             } else {
-                echo "Eroare la ștergerea vaccinului:" . mysqli_error($conn);
+                echo "Eroare la stergerea vaccinului:" . mysqli_error($conn);
             }
         } else {
-            echo "Nu există niciun vaccin cu ID-ul $id_vaccin!<br>";
+            echo "Nu exista nici un vaccin cu ID-ul $id_vaccin!<br>";
         }
     }
 
-    // Ștergere tuturor vaccinurilor
+    // Stergerea tuturor vaccinurilor
     if (isset($_POST["sterge_toate_vaccinurile"])) {
         $sql_delete_all = "DELETE FROM vaccinuri";
         if (mysqli_query($conn, $sql_delete_all)) {
-            echo "Toate datele din tabelul 'vaccinuri' au fost șterse cu succes!<br>";
+            echo "Toate datele din tabelul 'vaccinuri' au fost sterse cu succes!<br>";
             $reset_auto_increment = "ALTER TABLE vaccinuri AUTO_INCREMENT = 1";
             if (mysqli_query($conn, $reset_auto_increment)) {
                 echo "Incrementul a fost resetat cu succes!<br>";
@@ -154,11 +154,11 @@ if (mysqli_query($conn, $reset_auto_increment)) {
                 echo "Eroare la resetarea incrementului: " . mysqli_error($conn);
             }
         } else {
-            echo "Eroare la ștergerea datelor din tabelul 'vaccinuri': " . mysqli_error($conn);
+            echo "Eroare la stergerea datelor din tabelul 'vaccinuri': " . mysqli_error($conn);
         }
     }
 
-    // Selectare și afișare vaccinuri
+    // Selectare si afisare vaccinuri
     $sql_select_vaccinuri = "SELECT * FROM vaccinuri";
     $result = $conn->query($sql_select_vaccinuri);
 
@@ -168,10 +168,10 @@ if (mysqli_query($conn, $reset_auto_increment)) {
             echo "ID: " . $row["id"]. " - Nume: " . $row["nume"]. " - Tip: " . $row["tip"]  .  "<br>";
         }
     } else if(isset($_POST["sterge_vaccin"])) {
-        echo "Nu au fost găsite înregistrări în tabelul 'vaccinuri'!<br>";
+        echo "Nu au fost gasite inregistrari in tabelul 'vaccinuri'!<br>";
     }
 
-    // Resetează incrementul dacă nu există înregistrări în tabelul vaccinuri
+    // Resetează incrementul daca nu exista inregistrari in tabelul vaccinuri
     if ($result->num_rows == 0 &&  isset($_POST["sterge_vaccin"])) {
         $reset_auto_increment = "ALTER TABLE vaccinuri AUTO_INCREMENT = 1";
         if (mysqli_query($conn, $reset_auto_increment)) {
@@ -182,7 +182,6 @@ if (mysqli_query($conn, $reset_auto_increment)) {
     }
 
   // Vizualizare animale si vaccinuri disponibile
-
   $sql_select_animale = "SELECT * FROM animale";
   $result = $conn->query($sql_select_animale);
   
@@ -193,7 +192,7 @@ if (mysqli_query($conn, $reset_auto_increment)) {
             echo " ID: " . $row["id"]. " - Nume: " . $row["nume"]. " - Specie: " . $row["specie"]. " - Varsta: " .$row["varsta"] . "<br>";
           }
       } else {
-          echo "Nu au fost găsite înregistrări în tabelul 'animale'!<br>";
+          echo "Nu au fost gasite inregistrari in tabelul 'animale'!<br>";
       }
 
       $sql_select_vaccinuri = "SELECT * FROM vaccinuri";
@@ -205,7 +204,7 @@ if (mysqli_query($conn, $reset_auto_increment)) {
             echo "ID: " . $row["id"]. " - Nume: " . $row["nume"]. " - Tip: " . $row["tip"]  .  "<br>";
         }
     } else {
-        echo "Nu au fost găsite înregistrări în tabelul 'vaccinuri'!<br>";
+        echo "Nu au fost gasite inregistrari in tabelul 'vaccinuri'!<br>";
     }
 
     $sql_select_administrare_vaccinuri = "SELECT * FROM administrare_vaccinuri";
@@ -240,47 +239,47 @@ if (mysqli_query($conn, $reset_auto_increment)) {
             $id_vaccin = $_POST["id_vaccin_ad"];
             $data_aplicare = $_POST["data_aplicare"];
         
-            // Verificăm dacă animalul există în baza de date
+            // Verificam daca animalul exista in baza de date
             $animal_query = "SELECT id FROM animale WHERE id = $id_animal";
             $animal_result = mysqli_query($conn, $animal_query);
             if (mysqli_num_rows($animal_result) == 0) {
-                echo "Animalul specificat nu există în baza de date.";
-                exit; // Ieșim din script dacă animalul nu există
+                echo "Animalul specificat nu exista in baza de date.";
+                exit; // Iesim din script daca animalul nu exista
             } 
             
         
-            // Verificăm dacă vaccinul există în baza de date
+            // Verificam daca vaccinul exista in baza de date
             $vaccin_query = "SELECT id FROM vaccinuri WHERE id = $id_vaccin";
             $vaccin_result = mysqli_query($conn, $vaccin_query);
             if (mysqli_num_rows($vaccin_result) == 0) {
-                echo "Vaccinul specificat nu există în baza de date.";
-                exit; // Ieșim din script dacă vaccinul nu există
+                echo "Vaccinul specificat nu exista in baza de date.";
+                exit; // Iesim din script daca vaccinul nu exista
             }
         
-            // Inserarea datelor în tabelul de administrare a vaccinurilor
+            // Inserarea datelor in tabelul de administrare a vaccinurilor
 $sql = "INSERT INTO administrare_vaccinuri (id_animal, id_vaccin, data_aplicare) VALUES ('$id_animal', '$id_vaccin', '$data_aplicare')";
 if ($conn->query($sql) === TRUE) {
-    // Interogarea pentru a obține numele animalului
+    // Interogarea pentru a obtine numele animalului
     $animal_query = "SELECT nume FROM animale WHERE id = $id_animal";
     $result_animal = $conn->query($animal_query);
     
-    // Interogarea pentru a obține numele vaccinului
+    // Interogarea pentru a obtine numele vaccinului
     $vaccin_query = "SELECT nume FROM vaccinuri WHERE id = $id_vaccin";
     $result_vaccin = $conn->query($vaccin_query);
     
-    // Verificăm dacă ambele interogări au returnat rezultate
+    // Verificam daca ambele interogari au returnat rezultate
     if ($result_animal->num_rows > 0 && $result_vaccin->num_rows > 0) {
-        // Extragem datele din rândurile rezultate
+        // Extragem datele din randurile rezultate
         $row_animal = $result_animal->fetch_assoc();
         $row_vaccin = $result_vaccin->fetch_assoc();
         
-        // Afișăm mesajul de succes folosind numele animalului și al vaccinului
+        // Afisam mesajul de succes folosind numele animalului si al vaccinului
         echo "Administrarea vaccinului " . "'" .$row_vaccin["nume"] . "'" ." pentru animalul " . "'" .$row_animal["nume"] . "'" . " la data de '$data_aplicare' a fost realizată cu succes!";
     } else {
-        echo "Nu s-au găsit înregistrări pentru ID-urile specificate.";
+        echo "Nu s-au gasit inregistrari pentru ID-urile specificate.";
     }
 } else {
-    // Afișăm eroarea specifică pentru interogarea de inserare
+    // Afisam eroarea specifica pentru interogarea de inserare
     echo "Eroare la administrarea vaccinului: " . $sql . "<br>" . $conn->error;
    }
   }
@@ -288,15 +287,15 @@ if ($conn->query($sql) === TRUE) {
   if (isset($_POST["sterge_administrarea"])) {
     $id_administrare = $_POST["id_administrare"];
 
-    // Verificăm dacă ID-ul există în tabel
+    // Verificam daca ID-ul exista in tabel
     $check_query = "SELECT id FROM administrare_vaccinuri WHERE id = $id_administrare";
     $check_result = mysqli_query($conn, $check_query);
 
     if (mysqli_num_rows($check_result) > 0) {
-        // ID-ul există în tabel, efectuăm operația de ștergere
+        // ID-ul exista in tabel, efectuam operatia de stergere
         $delete_query = "DELETE FROM administrare_vaccinuri WHERE id = $id_administrare";
         if (mysqli_query($conn, $delete_query)) {
-            echo "Administrarea cu ID-ul $id_administrare a fost ștearsa cu succes!<br>";
+            echo "Administrarea cu ID-ul $id_administrare a fost stearsa cu succes!<br>";
             $sql_select_administrare_vaccinuri = "SELECT * FROM administrare_vaccinuri";
             $result = $conn->query($sql_select_administrare_vaccinuri);
         if ($result->num_rows == 0) {
@@ -308,17 +307,17 @@ if ($conn->query($sql) === TRUE) {
             }
         }
         } else {
-            echo "Eroare la ștergerea administrarii: " . mysqli_error($conn);
+            echo "Eroare la stergerea administrarii: " . mysqli_error($conn);
         }
     } else {
-        // ID-ul nu există în tabel, afișăm un mesaj corespunzător
-        echo "Nu există nici o administrare cu ID-ul $id_administrare!<br>";
+        // ID-ul nu exista in tabel, afisam un mesaj corespunzător
+        echo "Nu exista nici o administrare cu ID-ul $id_administrare!<br>";
         
     }
 
   }
 } 
 
-// Închide conexiunea
+// Inchide conexiunea
 $conn->close();
 
